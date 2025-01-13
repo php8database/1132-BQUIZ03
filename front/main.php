bc9bbcd14fcb42a1fe51a7aab8b155b8648d6d6c
php8database <php8database@gmail.com>
2025-01-13 10:32:41 +0800

    完成點選按鈕更換海報及轉場效果

--- front/main.php
+++ front/main.php
@@ -112,10 +112,14 @@ let slider=setInterval(() => {
     sliders();
 }, 2500);
 
-function sliders(){
+
+function sliders(next=-1){
     let now=$(".poster:visible").index();
-    let next=($(".poster").length==now+1)?0:now+1;
+    if(next==-1){
+        next=($(".poster").length==now+1)?0:now+1;
+    }
     let ani=$(".poster").eq(next).data('ani');
+    console.log(now,next,ani)
     //console.log(now,next)
 
     switch(ani){
@@ -170,7 +174,10 @@ $(".icons").hover(
     }
 )
 
-
+$(".icon").on("click",function(){
+    let next=$(this).index();
+    sliders(next);
+})
 </script>
 
 
