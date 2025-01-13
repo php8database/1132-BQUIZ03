c02724390e345e805097d6e4b3200f842b1c08e5
php8database <php8database@gmail.com>
2025-01-13 14:41:09 +0800

    製作畫面切換機制

--- front/order.php
+++ front/order.php
@@ -21,6 +21,7 @@
         background:#999;
     }
 </style>
+<div id="order">
 <h3 class='ct'>線上訂票</h3>
 <form action="#">
 <table class="order-form">
@@ -38,12 +39,18 @@
     </tr>
     <tr>
         <td colspan='2' class='ct'>
-            <input type="button" value="確定">
+            <input type="button" value="確定" onclick="$('#order,#booking').toggle()">
             <input type="reset" value="重置">     
         </td>
     </tr>
 </table>
 </form>
+</div>
+<div id="booking" style="display:none">
+
+畫位
+<button  onclick="$('#order,#booking').toggle()">上一步</button>
+</div>
 
 <script>
 getMovies();
