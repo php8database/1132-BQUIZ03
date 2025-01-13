f9b850c410594bef7184beaf6042583365476cde
php8database <php8database@gmail.com>
2025-01-13 09:24:30 +0800

    完成按鈕左右平滑移動動畫效果

--- front/main.php
+++ front/main.php
@@ -144,13 +144,19 @@ function sliders(){
 let total=$(".icon").length;
 let p=0;
 $(".left,.right").on("click",function(){
-    if((p+1)<=(total-4)){
-        p++;
-        $(".icon").animate({right:80*p});
+    if($(this).hasClass('left')){
+        /* if(p-1>=0){
+            p--;
+        } */
+        p=(p-1>=0)?p-1:0;
+    }else{
+        /* if(p+1<=total-4){
+            p++;
+        } */
+        p=(p+1<=total-4)?p+1:total-4;
     }
-    
 
-
+    $(".icon").animate({right:p*80}); 
 })
 
 
