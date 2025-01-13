96518c4e507c28ce971ed7d64a90ab2786ca072c
php8database <php8database@gmail.com>
2025-01-13 15:26:57 +0800

    示範切換畫面前如何取得表單值及在新畫面中使用

--- front/order.php
+++ front/order.php
@@ -39,7 +39,7 @@
     </tr>
     <tr>
         <td colspan='2' class='ct'>
-            <input type="button" value="確定" onclick="$('#order,#booking').toggle()">
+            <input type="button" value="確定" onclick="booking()">
             <input type="reset" value="重置">     
         </td>
     </tr>
@@ -48,15 +48,14 @@
 </div>
 <div id="booking" style="display:none">
 
-畫位
-<button  onclick="$('#order,#booking').toggle()">上一步</button>
+
 </div>
 
 <script>
 getMovies();
 let id=new URLSearchParams(location.href).get('id');
 //console.log(id);
-
+let movie={};
 $("#movie").on("change",function(){
     getDays();
 })
@@ -67,7 +66,7 @@ $("#date").on("change",function(){
 
 function getMovies(){
     $.get("api/get_movies.php",function(movies){
-        console.log(movies);
+        //console.log(movies);
         $("#movie").html(movies);
 
         if(parseInt(id)>0){
@@ -89,6 +88,19 @@ function getSessions(){
     $.get("api/get_sessions.php",{movie:$("#movie").val(),date:$("#date").val()},function(sessions){
         $("#session").html(sessions);
     })
+
+}
+
+function booking(){
+    movie={id:$("#movie").val(),
+           name:$("#movie option:selected").text(),
+           date:$("#date").val(),
+           session:$("#session").val()
+    }
+
+    $("#booking").html(`${movie.id},${movie.date},${movie.name},<button  onclick="$('#order,#booking').toggle()">上一步</button>`)
+
+    $("#booking,#order").toggle();
 
 }
 
