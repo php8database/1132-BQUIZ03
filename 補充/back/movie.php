fd8aaa8f17f8041cfdf207bc2da0ea349cabce98
php8database <php8database@gmail.com>
2025-02-27 10:52:45 +0800

    將刪除電影改成直接前端移除dom,而不用重新載入電影列表

--- 補充/back/movie.php
+++ 補充/back/movie.php
@@ -30,8 +30,11 @@ function getMovies() {
 
         $(".del").on("click", function() {
             let id = $(this).data('id');
+            $movie=$(this).parents(".movie-item");
+            //console.log($($movie).html());
             $.post("./api/del.php", { table: 'Movie', id}, () => {
-                getMovies();
+                //getMovies();
+                $($movie).remove();
             })
         })
     });
