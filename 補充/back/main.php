91e5247c5b3debc22088b0f76884bc2ba4bb8ed6
php8database <php8database@gmail.com>
2025-02-27 10:55:31 +0800

    預告版後台改以ajax載入列表來改善使用者體驗

--- 補充/back/main.php
+++ 補充/back/main.php
@@ -8,32 +8,31 @@ if(isset($_POST['acc'])){
 }
 
 
-
 if(!isset($_SESSION['login'])){
 ?>
 <form action="?" method='post'>
-<table class="tab" style="width:300px;">
-    <tr>
-        <td>帳號：</td>
-        <td><input type="text" name="acc" id="acc"></td>
-    </tr>
-    <tr>
-        <td>密碼：</td>
-        <td><input type="password" name="pw" id="pw"></td>
-    </tr>
-</table>
-<div class="ct">
-    <input type="submit" value="登入">
-    <input type="reset" value="重置">
-</div>
+    <table class="tab" style="width:300px;">
+        <tr>
+            <td>帳號：</td>
+            <td><input type="text" name="acc" id="acc"></td>
+        </tr>
+        <tr>
+            <td>密碼：</td>
+            <td><input type="password" name="pw" id="pw"></td>
+        </tr>
+    </table>
+    <div class="ct">
+        <input type="submit" value="登入">
+        <input type="reset" value="重置">
+    </div>
 
-<?php
+    <?php
 }else{
 ?>
     <div class="rb tab">
-          <h2 class="ct">請選擇所需功能</h2>
-        </div>
-<?php         
+        <h2 class="ct">請選擇所需功能</h2>
+    </div>
+    <?php         
 }
 ?>
 </form>
--- 補充/back/poster.php
+++ 補充/back/poster.php
@@ -1,61 +1,61 @@
 <div style="height:340px;">
     <h3 class='ct'>預告片清單</h3>
-<div style="display:flex; justify-content:space-between;text-align:center
+    <div style="display:flex; justify-content:space-between;text-align:center
 ">
-    <div style="width:25%">預告片海報</div>
-    <div style="width:25%">預告片片名</div>
-    <div style="width:25%">預告片排序</div>
-    <div style="width:24%">操作</div>
-</div>
+        <div style="width:25%">預告片海報</div>
+        <div style="width:25%">預告片片名</div>
+        <div style="width:25%">預告片排序</div>
+        <div style="width:24%">操作</div>
+    </div>
 
-<form action="./api/edit_poster.php" method="post">
-<div style="overflow:auto;height:210px;" id="postList">
+    <form action="./api/edit_poster.php" method="post">
+        <div style="overflow:auto;height:210px;" id="postList">
+        </div>
+    </form>
 </div>
-</form>
-</div>
-
 
 
 <script>
 getPosters();
 
-function getPosters(){
-/*     $.get("./back/poster_list.php",function(res){
-        $("#postList").html(res);
-    }) */
+function getPosters() {
+    /*     $.get("./back/poster_list.php",function(res){
+            $("#postList").html(res);
+        }) */
 
-   $("#postList").load("./back/poster_list.php",function(){
-    $(".sw").on("click",function(){
-        let id=$(this).data('id');
-        let sw=$(this).data('sw');
-        $.post("./api/sw.php",{table:'Poster',id,sw},()=>{
-            getPosters();
+    $("#postList").load("./back/poster_list.php", function() {
+        $(".sw").on("click", function() {
+            let id = $(this).data('id');
+            let sw = $(this).data('sw');
+            $.post("./api/sw.php", {
+                table: 'Poster',
+                id,
+                sw
+            }, () => {
+                getPosters();
+            })
         })
-})
-   });
+    });
 }
-
-
-
 </script>
 
 <hr>
 <div style="height:170px;">
-<h3 class='ct'>新增預告片海報</h3>
-<form action="./api/add_poster.php" method="post" enctype="multipart/form-data">
-    <table>
-        <tr>
-            <td>預告片海報：</td>
-            <td><input type="file" name="img" id=""></td>
-            <td>預告片片名：</td>
-            <td><input type="text" name="name" id=""></td>
-        </tr>
-    </table>
-    <div class="ct">
-        <input type="submit" value="新增">
-        <input type="reset" value="重置">
-    </div>
-</form>
+    <h3 class='ct'>新增預告片海報</h3>
+    <form action="./api/add_poster.php" method="post" enctype="multipart/form-data">
+        <table>
+            <tr>
+                <td>預告片海報：</td>
+                <td><input type="file" name="img" id=""></td>
+                <td>預告片片名：</td>
+                <td><input type="text" name="name" id=""></td>
+            </tr>
+        </table>
+        <div class="ct">
+            <input type="submit" value="新增">
+            <input type="reset" value="重置">
+        </div>
+    </form>
 
 
 </div>
